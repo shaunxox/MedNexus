8469eb18d55db652ed0b2099c04f1e2bc884218b
Tanish <tanishpurusha2006@gmail.com>
2026-03-20 14:36:30 +0530

    fix: update API URL to production backend

--- doctor/script.js
+++ doctor/script.js
@@ -8,7 +8,7 @@ function logout() {
   window.location.href = '../index.html';
 }
 
-const API = 'http://localhost:5000';
+const API = 'https://mednexus-tuz3.onrender.com';
 let medicineCount = 1;
 let selectedToken = null;
 
